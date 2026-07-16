Added Types (and other small fixes) (need to add "rotation" and "scale" to Types)

src/Py3DRF/backends/plotly/scene.py
@@ -49,7 +49,7 @@ class ScenePlotly(SceneBackend):
         self.figure.update_layout(
             scene=dict(
                 aspectmode="data",
-                xaxis=dict(visible=not transparent or True),
+                xaxis=dict(visible=not transparent),
             ),
             paper_bgcolor="rgba(0,0,0,0)" if transparent else "white",
             margin=dict(l=0, r=0, t=0, b=0),
@@ -80,9 +80,9 @@ class ScenePlotly(SceneBackend):
         :return: The plotly camera dict that was applied to the figure.
 
         """
-        location = np.asarray(camera.location, dtype=float)
+        location = camera.location
         camera_dict = dict(
-            eye=dict(x=float(location[0]), y=float(location[1]), z=float(location[2])),
+            eye=dict(x=float(location.x), y=float(location.y), z=float(location.z)),
             center=dict(x=0.0, y=0.0, z=0.0),
             up=dict(x=0.0, y=0.0, z=1.0),
         )

src/Py3DRF/core/camera.py
@@ -1,11 +1,12 @@
 import numpy as np
+from .types import Location
 
 class Camera:
     """
     Camera class representing the Blender camera object.
     """
 
-    def __init__(self, name="Camera", location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1), focal_length=50):
+    def __init__(self, name="Camera", location=Location(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1), focal_length=50):
         """
 
         :param name: Name of the camera object.
@@ -57,7 +58,7 @@ class Camera:
         """
         self.scale = scale
 
-    def focusOnPoint(self, point, azimuth=np.pi/4, elevation=np.pi/9, distance=3):
+    def focusOnPoint(self, point :Location, azimuth=np.pi/4, elevation=np.pi/9, distance=3):
         """
         Reposition the camera to focus on a point.
 
@@ -70,7 +71,7 @@ class Camera:
         x = distance * np.cos(azimuth) * np.cos(elevation)
         y = distance * np.sin(azimuth) * np.cos(elevation)
         z = distance * np.sin(elevation)
-        coords = np.array((x, y, z)) + point
+        coords = np.array((x, y, z)) + point.to_array()
         rotation = (np.pi/2 - elevation, 0, azimuth + np.pi/2)
-        self.setLocation(coords)
+        self.setLocation(Location(*coords))
         self.setRotation(rotation)

src/Py3DRF/core/mesh.py
@@ -3,7 +3,7 @@ import numpy as np
 from .materials import Material
 from .camera import Camera
 from .pointcloud import PointCloudSettings
-
+from .types import Location
 
 class Mesh:
     """
@@ -20,7 +20,7 @@ class Mesh:
             vertices=[],
             edges=[],
             faces=[],
-            location=(0, 0, 0),
+            location=Location(0, 0, 0),
             rotation=(0, 0, 0),
             scale=(1, 1, 1),
             material: Material = None,
@@ -45,7 +45,7 @@ class Mesh:
         self.edges = edges
         self.faces = faces
 
-        self.location = np.array(location, dtype=float)
+        self.location = location
         self.rotation = np.array(rotation, dtype=float)
         self.scale = np.array(scale, dtype=float)
 
@@ -104,7 +104,7 @@ class Mesh:
 
         matrix = np.eye(4)
         matrix[:3, :3] = rotation_matrix @ scale_matrix
-        matrix[:3, 3] = self.location
+        matrix[:3, 3] = self.location.to_tuple()
         return matrix
 
     def _worldVertices(self):
@@ -144,15 +144,14 @@ class Mesh:
             return -float('inf')
         return float(world[:, 2].max())
 
-    def setLocation(self, location=(0, 0, 0)):
+    def setLocation(self, location=Location(0, 0, 0)):
         """
         Set the location of the object with respect to the global reference frame.
 
         :param location: Vector of coordinates representing the new location of the object.
 
         """
-        self.location = np.array(location, dtype=float)
-
+        self.location = location.to_array()
     def setRotation(self, rotation=(0, 0, 0)):
         """
         Set the rotation of the object with pivot point the origin of the local reference frame.
@@ -181,16 +180,16 @@ class Mesh:
 
         """
         minz = self.getMinZ()
-        verices = [
-            (-size[0]/2, -size[0]/2, 0),
-            (-size[0]/2, size[0]/2, 0),
-            (size[0]/2, size[0]/2, 0),
-            (size[0]/2, -size[0]/2, 0),
+        vertices = [
+            (-size[0]/2, -size[1]/2, 0),
+            (-size[0]/2, size[1]/2, 0),
+            (size[0]/2, size[1]/2, 0),
+            (size[0]/2, -size[1]/2, 0),
         ]
         faces = [
             (0, 1, 2, 3)
         ]
-        floor = Mesh("Floor", vertices=verices, faces=faces, location=(0, 0, minz))
+        floor = Mesh("Floor", vertices=vertices, faces=faces, location=Location(0, 0, minz))
         floor.is_shadow_catcher = shadow_catcher
         return floor
 